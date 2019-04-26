5fbaba21d767990e0c7a63b60672f5052c36d6d7
gudza <henryclash@gmail.com>
2019-04-26 23:10:12 +0300

    names of pics bug

--- engine/app.js
+++ engine/app.js
@@ -220,7 +220,7 @@ class PlaneAvatar extends THREE.Mesh {
 
 constructor(Group,AnchorPointIndex,picindex) {
 
-	const texture = new THREE.TextureLoader().load( "userpics/"+picindex+".png" );
+	const texture = new THREE.TextureLoader().load( "userpics/Frame-"+picindex+".png" );
 	super(new THREE.CircleGeometry(0.4,32,32),new THREE.MeshBasicMaterial( { map: texture} ));
 	this.name = AnchorPointIndex; 
 	this.dissolving = true; //Dissolving by default
